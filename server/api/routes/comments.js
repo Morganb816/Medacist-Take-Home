9d102cf477b0afaed97a3c0383f2a9c1b1ec1ade
Morgan Brown <morganb816@gmail.com>
2021-03-07 10:43:47 -0500

    feat: exported postman collection to repo

--- server/api/routes/comments.js
+++ server/api/routes/comments.js
@@ -1,11 +1,14 @@
-const express = require('express');
 const { getCommentsForPostService, getCommentService, makeCommentService, makeLikeService, unlikeService } = require('../services/comments');
+const { validateSchema } = require('../middleware');
+const express = require('express');
+const CommentSchema = require('../schemas/Comment.json');
+const LikeSchema = require('../schemas/Like.json');
 const router = express.Router();
 
 router.get('/post/:postId', getCommentsForPostService);
 router.get('/:commentId', getCommentService);
-router.post('/', makeCommentService);
-router.post('/like', makeLikeService);
-router.delete('/like', unlikeService);
+router.post('/', [validateSchema(CommentSchema)], makeCommentService);
+router.post('/like', [validateSchema(LikeSchema)], makeLikeService);
+router.delete('/like', [validateSchema(LikeSchema)], unlikeService);
 
 module.exports = router;
